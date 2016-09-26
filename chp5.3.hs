f6f4d3f8160d95e454d7a857f8381b9e757cfe8e
Gabriel <gab@fastmail.net>
2016-09-26 21:56:12 +0200

    Add files via upload

--- chp5.3.hs
+++ chp5.3.hs
@@ -1,3 +1,6 @@
+grid :: Int -> Int -> [(Int,Int)]
+grid m n = [(x,y) | x <- [0..n], y <- [0..m]]
+
 square :: Int -> [(Int,Int)]
-square n = [(x,y) | x <- [0..n], y <- [0..n], x /= y]
+square n = [(v,k) | (v,k) <- grid n n, v /= k]
 
