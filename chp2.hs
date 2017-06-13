59c009bf1497b150f9d79f780277efe3dd1d6ada
Gabriel Ivanes <gab@fastmail.net>
2017-06-13 20:42:54 +0200

    in progress

--- chp2.hs
+++ chp2.hs
@@ -12,3 +12,11 @@ numb = a `div` length xs
         xs = [1,2,3,4,5]
 
 -- 2.4
+last :: Num t => [t] -> t
+last [] = 0
+-- last xs = xs !! (length xs - 1)
+last xs = head (reverse xs) 
+
+init [] = []
+--init xs = take (length xs - 1) xs
+init xs = reverse (tail (reverse xs)) 
