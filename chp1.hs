70db305529343a9dfd4025b1b3b1194d83a24017
Gabriel Ivanes <gab@fastmail.net>
2017-06-13 14:18:40 +0200

    seqn

--- chp1.hs
+++ chp1.hs
@@ -4,3 +4,8 @@ qsort (x:xs) = qsort smaller ++ [x] ++ qsort larger
                where
                   smaller = [a | a <- xs, a <= x]
                   larger  = [b | b <- xs, b > x]
+
+seqn []         = return []
+seqn (act:acts) = do x <- act
+                       xs <- seqn acts
+                       return (x:xs)
